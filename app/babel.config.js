c0e39a4c795406d173868f571511a72f9d9b67ad
turnoffthiscomputer <colin.remi07@gmail.com>
2024-10-30 17:07:17 +0100

    add flash-list and bottom-sheet

--- app/babel.config.js
+++ app/babel.config.js
@@ -2,5 +2,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     ["@babel/plugin-transform-private-methods", { "loose": true }],
+    'react-native-reanimated/plugin',
   ],
 };
--- app/index.js
+++ app/index.js
@@ -2,12 +2,15 @@
  * @format
  */
 
+import 'react-native-reanimated';
 import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { TamaguiProvider, createTamagui } from 'tamagui';
 import { config } from '@tamagui/config/v2-native'
 import { ToastProvider } from '@tamagui/toast';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 const tamaguiConfig = createTamagui(config)
 
 LogBox.ignoreLogs([
@@ -17,12 +20,13 @@ LogBox.ignoreLogs([
 ])
 
 const Root = () => (
-  <TamaguiProvider config={tamaguiConfig}>
-    <ToastProvider swipeDirection="up">
-      <App />
-    </ToastProvider>
-  </TamaguiProvider>
-
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <TamaguiProvider config={tamaguiConfig}>
+      <ToastProvider swipeDirection="up">
+        <App />
+      </ToastProvider>
+    </TamaguiProvider>
+  </GestureHandlerRootView>
 );
 
 AppRegistry.registerComponent(appName, () => Root);
